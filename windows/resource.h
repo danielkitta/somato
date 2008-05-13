0fd08cc1a1c40f158a905d5a029813b0b8c42d2c
Daniel Elstner <daniel.kitta@gmail.com>
2008-05-13 23:44:44 +0000

    Remove all custom resources from the resource script and delete ui/cubetexture.bmp. Move ui/somato.ico to windows/ subdirectory since the integrated resource editor of Visual Studio does not honor the include path property set in the build configuration.

--- windows/resource.h
+++ windows/resource.h
@@ -3,18 +3,13 @@
 // Used by somato.rc
 //
 #define IDI_APPICON                     101
-#define IDB_CUBETEXTURE                 102
-#define IDR_UIMANAGER_MAINWINDOW        103
-#define IDR_GLADE_MAINWINDOW            104
-#define IDR_GTKRC_CUBESCENE             105
-#define UTF8TEXT                        257
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           102
